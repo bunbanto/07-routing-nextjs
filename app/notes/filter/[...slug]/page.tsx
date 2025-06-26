1c8fd828cbb48f3310523f6825ffc73e45167496
yurii_fishbakh <bunbanto@gmail.com>
2025-06-26 18:00:34 +0200

    tagmanu fixed

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,11 +1,11 @@
 import { fetchNotes as fetchNotesApi } from '@/lib/api';
 import NoteClient from './Notes.client';
 
-interface PageProps {
-  params: { slug: string[] };
+interface Props {
+  params: Promise<{ slug: string[] }>;
 }
 
-const Page = async ({ params }: PageProps) => {
+const Page = async ({ params }: Props) => {
   const { slug } = await params;
   const category = slug[0] === 'all' ? undefined : slug[0];
   const response = await fetchNotesApi({ page: 1, search: '', tag: category });
