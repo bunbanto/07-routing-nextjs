88d8ca929b5dbb368b76b145bda9d92b76f004bb
yurii_fishbakh <bunbanto@gmail.com>
2025-06-27 11:29:05 +0200

    feixed NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,15 +1,34 @@
 'use client';
 
-import { useParams } from 'next/navigation';
-import NotePreview from '@/components/NotePreview/NotePreview';
+import { useParams, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 import Modal from '@/components/Modal/Modal';
+import NotePreview from '@/components/NotePreview/NotePreview';
+import Loader from '@/components/Loader/Loader';
+import { getNoteById } from '@/lib/api';
+import type { Note } from '@/types/note';
 
 export default function NoteDetailsClient() {
-  const { id } = useParams<{ id: string }>();
+  const { id: numId } = useParams<{ id: string }>();
+  const router = useRouter();
+
+  const id = Number(numId);
+  const handleClose = () => router.back();
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery<Note>({
+    queryKey: ['note', id],
+    queryFn: () => getNoteById(Number(id)),
+  });
 
   return (
-    <Modal>
-      <NotePreview id={id} />
+    <Modal onClose={handleClose}>
+      {isLoading && <Loader />}
+      {error && <p>Error loading note</p>}
+      {note && <NotePreview id={id.toString()} />}
     </Modal>
   );
 }
